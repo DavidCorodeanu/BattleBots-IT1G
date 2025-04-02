086b428d70803d7166486c253b18572aed619ce6
DavidCorodeanu <david.corodeanu@student.nhlstenden.com>
2025-04-02 09:44:19 +0200

    changes RGB colors

--- SecondTrack_Corodeanu_David/Libraries/debugLight.h
+++ SecondTrack_Corodeanu_David/Libraries/debugLight.h
@@ -44,35 +44,35 @@ void setDriveBackwardColor()
 {
     NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 0, 0, 0);
     NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 0, 0, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 0, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 0, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 255, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 255, 0);
     NeoPixel.show();
 }
 
 void setTurnRightColor()
 {
     NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 0, 0, 0);
-    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 255, 100, 0);
+    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 255, 150, 0);
     NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 0, 0, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 100, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 150, 0);
     NeoPixel.show();
 }
 
 void setTurnLeftColor()
 {
-    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 255, 100, 0);
+    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 255, 150, 0);
     NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 0, 0, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 100, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 150, 0);
     NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 0, 0, 0);
     NeoPixel.show();
 }
 
 void setTurnAroundColor()
 {
-    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 255, 100, 0);
-    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 255, 100, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 100, 0);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 100, 0);
+    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 255, 150, 0);
+    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 255, 150, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 255, 150, 0);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 255, 150, 0);
     NeoPixel.show();
 }
 
@@ -87,9 +87,9 @@ void setDriveStopColor()
 
 void setCalibrateColor()
 {
-    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 0, 85, 255);
-    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 0, 85, 255);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 0, 85, 255);
-    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 0, 85, 255);
+    NeoPixel.setPixelColor(PIXEL_TOP_LEFT, 0, 255, 255);
+    NeoPixel.setPixelColor(PIXEL_TOP_RIGHT, 0, 255, 255);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_LEFT, 0, 255, 255);
+    NeoPixel.setPixelColor(PIXEL_BOTTOM_RIGHT, 0, 255, 255);
     NeoPixel.show();
 }
